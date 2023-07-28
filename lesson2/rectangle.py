df53fe8fc0c6cb8d100fd165715d734a3eac586f
u_alex90 <u_alex90@mail.ru>
2023-07-28 23:47:15 +1000

    processed Lesson2 rectangle

--- lesson2/rectangle.py
+++ lesson2/rectangle.py
@@ -4,14 +4,35 @@
 - __init__(self, width, height): конструктор, принимающий ширину и высоту прямоугольника
 - area(self): метод, возвращающий площадь прямоугольника
 - perimeter(self): метод, возвращающий периметр прямоугольника
-- from_diagonal(cls, diagonal, aspect_ratio): класс-метод, принимающий диагональ прямоугольника и соотношение сторон и возвращающий объект класса Rectangle
+- from_diagonal(cls, diagonal, aspect_ratio): класс-метод, принимающий диагональ прямоугольника и соотношение сторон и
+  возвращающий объект класса Rectangle
 - is_square(width, height): статический метод, принимающий ширину и высоту прямоугольника и возвращающий True,
 если это квадрат, и False в противном случае
 """
+from math import degrees, tan, atan, cos, sin, asin
 
 
 class Rectangle:
-    pass
+    def __init__(self, width: float, height: float) -> None:
+        self.width = width
+        self.height = height
+        
+    def area(self) -> float:
+        return self.width * self.height
+
+    def perimeter(self) -> float:
+        return (self.width + self.height) * 2
+    
+    @classmethod
+    def from_diagonal(cls, diagonal: float, aspect_ratio: float) -> 'Rectangle':
+        pass
+        # alpha = degrees(atan(1 / aspect_ratio))
+        # print(alpha)
+        # return cls(1,1)
+
+    @staticmethod
+    def is_square(width: float, height: float) -> bool:
+        return True if width == height else False
 
 
 rectangle = Rectangle(4, 5)
@@ -21,6 +42,7 @@ print(rectangle.perimeter())  # 18
 rectangle2 = Rectangle.from_diagonal(5, 2)
 print(rectangle2.area())  # 10.0128
 print(rectangle2.perimeter())  # 13.42
-
+print(rectangle2.width)
+print(rectangle2.height)
 print(Rectangle.is_square(4, 4))  # True
 print(Rectangle.is_square(4, 5))  # False
