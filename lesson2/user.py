e30969e34a1d3a3e6b758c918f956c60ba7e585b
u_alex90 <u_alex90@mail.ru>
2023-07-29 10:43:43 +1000

    solved Lesson2 user

--- lesson2/user.py
+++ lesson2/user.py
@@ -7,14 +7,45 @@
 - is_admin: свойство, которое возвращает, является ли пользователь администратором или нет
 - _is_admin: свойство-помощник, которое определяет, является ли пользователь администратором или нет
 - login(self, password): метод, который проверяет, соответствует ли введенный пароль паролю пользователя
-- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False при условии, что пользователь залогинен)
+- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False
+  при условии, что пользователь залогинен)
 
 Для свойств name и password используйте декораторы @property и @password.setter.
 """
 
 
 class User:
-    pass
+    def __init__(self, name: str, password: str) -> None:
+        self.__name = name
+        self.__password = password
+        self._is_admin = False
+        self._is_logged_in = False
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def password(self) -> str:
+        return self.__password
+
+    @password.setter
+    def password(self, password: str) -> None:
+        self.__password = password
+
+    @property
+    def is_admin(self) -> bool:
+        return self._is_admin
+
+    def login(self, password: str) -> None:
+        if password == self.__password:
+            self._is_logged_in = True
+            print(True)
+        else:
+            print(False)
+
+    def logout(self) -> None:
+        self._is_logged_in = False
 
 
 user1 = User("Alice", "qwerty")
